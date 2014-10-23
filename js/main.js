3e9d8ace907e1696371b3a617466cf0d2465d953
Лемешко Олександр <sheriff_15@ukr.net>
2014-10-23 17:34:33 +0300

    header footer

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 $(function(){
-	
+
+	$('.js-menu').click(function() {
+	    var buttonMenu = $(this);
+	    var bigMenu = $('body').find('.big-menu');
+	    buttonMenu[buttonMenu.hasClass('active') ? 'removeClass' : 'addClass']('active');
+	    bigMenu[bigMenu.hasClass('active') ? 'removeClass' : 'addClass']('active');
+	});
+
 });
