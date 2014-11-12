e6facacb02fa6bfb766dd76aed0e62d6463d20bf
Лемешко Александр <sheriff_15@ukr.net>
2014-11-12 15:15:23 +0200

    календарик

--- js/directives.js
+++ js/directives.js
@@ -1,5 +1,5 @@
 /* Directives */
-angular.module('sushiApp', []).directive('counter', function() {
+angular.module('sushiApp', ['ui.bootstrap']).directive('counter', function() {
     return {
         restrict: 'A',
         scope: { value: '=value' },
@@ -93,4 +93,38 @@ angular.module('sushiApp', []).directive('counter', function() {
             };
         }
     }
+}).controller('DatepickerDemoCtrl', function ($scope) {
+  $scope.today = function() {
+    $scope.dt = new Date();
+  };
+  $scope.today();
+
+  $scope.clear = function () {
+    $scope.dt = null;
+  };
+
+  // Disable weekend selection
+  $scope.disabled = function(date, mode) {
+    return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
+  };
+
+  $scope.toggleMin = function() {
+    $scope.minDate = $scope.minDate ? null : new Date();
+  };
+  $scope.toggleMin();
+
+  $scope.open = function($event) {
+    $event.preventDefault();
+    $event.stopPropagation();
+
+    $scope.opened = true;
+  };
+
+  $scope.dateOptions = {
+    formatYear: 'yy',
+    startingDay: 1
+  };
+
+  $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+  $scope.format = $scope.formats[0];
 });
