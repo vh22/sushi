8b6391757b681d269e6300feddccdeabf643e2e3
Лемешко Александр <sheriff_15@ukr.net>
2014-11-19 15:33:04 +0200

    tooltip

--- js/controllers/tooltip.js
+++
@@ -1,4 +0,0 @@
-app.controller('TooltipCtrl', function ($scope) {
-  $scope.dynamicTooltip = 'Hello, World! Hello, World! Hello, World! Hello, World!Hello, World! Hello, World! Hello, World! Hello, World!';
-  $scope.dynamicTooltipText = 'dynamic';
-});
