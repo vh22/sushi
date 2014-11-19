9182ca4aea71d418309dcaba3a1a4744b92c7c30
Лемешко Александр <sheriff_15@ukr.net>
2014-11-19 10:05:34 +0200

    правки 19,11,14

--- js/controllers/DropdownCtrl.js
+++ js/controllers/DropdownCtrl.js
@@ -8,10 +8,19 @@ app.controller('DropdownCtrl', function ($scope) {
         {id: '5', name: 'Web-money', url: '../css/images/pay/webmoney.png'},
         {id: '6', name: 'Прочие электронные деньги', url: []},
     ];
+    $scope.countries = [
+        {id: '1', name: 'Калужская'},
+        {id: '2', name: 'Калужская2'},
+        {id: '3', name: 'Калужская1'},
+        {id: '4', name: 'Калужская3'},
+        {id: '5', name: 'Калужская4'},
+    ]
 
     $scope.selectedPayment = $scope.payment[0];
+    $scope.selectedCountry = $scope.countries[0];
 
     $scope.set = function(action) {
         $scope.selectedPayment = action;
+        $scope.selectedCountry = action;
     };
 });
